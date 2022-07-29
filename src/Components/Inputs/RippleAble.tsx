320d92637bf7445ce3e8dde701b0c473e7bdeff8
Grzegorz Juszczyk <gkjuszczyk@gmail.com>
2022-07-30 00:47:47 +0200

    Finally fixed ripples with refs

--- src/Components/Inputs/RippleAble.tsx
+++ src/Components/Inputs/RippleAble.tsx
@@ -1,50 +1,70 @@
-import { FunctionComponent, PropsWithChildren, useState } from 'react';
+import {
+  FunctionComponent,
+  PointerEventHandler,
+  PropsWithChildren,
+  useEffect,
+  useRef,
+  useState
+} from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import styled, { keyframes } from 'styled-components';
 import { ButtonProps, InputSize, RippleAbleProps } from '../Constants';
+import { useCancellables } from '../../Utils/UseCancelTimeouts';
 
 type Ripple = {
     key: number,
-    styles: any
+    styles: any,
+    vis: boolean
 }
 
 export const RippleAble: FunctionComponent<PropsWithChildren<RippleAbleProps>> = (props) => {
   const arr: Ripple[] = [];
   const [ripples, setRipples] = useState(arr);
-  const [lastKey, setLastKey] = useState(0);
+  const ripplesRef = useRef(arr);
+  const handleStates = useCancellables();
+  useEffect(() => { ripplesRef.current = ripples; }, [ripples]);
 
-  const handleDown = (e: any) => {
+  console.log(ripples.length, ripples);
+  const handleDown: PointerEventHandler = (e) => {
+    e.preventDefault();
     const target = e.currentTarget;
     const rect = target.getBoundingClientRect();
     const maxDim = Math.max(target.clientWidth, target.clientHeight);
     const styles: any = {};
     styles.left = `${e.clientX - rect.left - maxDim / 2}px`;
     styles.top = `${e.clientY - rect.top - maxDim / 2}px`;
-    styles.width = styles.height = maxDim;
-    const key = lastKey + 1;
-    setLastKey(key);
-    setRipples([...ripples, { key, styles }]);
+    styles.width = maxDim;
+    styles.height = maxDim;
+    let key: number = ripples[Math.abs(ripples.length - 1)]?.key || 0;
+    ++key;
+    setRipples([...ripples, { key, styles, vis: true }]);
   };
 
-  const handleUpOrLeave = (e: any) => {
+  const shiftRipples = () => {
+    console.log('shifting', ripplesRef.current);
+    setRipples(ripplesRef.current.slice(1));
+  };
+  const handleUpOrLeave: PointerEventHandler = (e) => {
+    e.preventDefault();
     const ripples2: Ripple[] = [...ripples];
     if (ripples2.length > 0) {
-      ripples2.shift();
+      ripples2[ripples2.length - 1].vis = false;
+      handleStates(
+        [[shiftRipples, 2000]],
+      );
     }
     setRipples(ripples2);
   };
 
   return (
-    <Wrapper {...props} onPointerDown={handleDown} onPointerLeave={handleUpOrLeave} onPointerUp={handleUpOrLeave}>
+    <Wrapper {...props} onPointerDown={handleDown} onPointerLeave={(e) => { handleUpOrLeave(e); }} onPointerUp={(e) => { handleUpOrLeave(e); }}>
       {props.children}
-      <WrapperInner {...props}>
-        <AnimatePresence>
+      <WrapperInner id="wrap" {...props}>
           {ripples.map((ripple) => (
-            <motion.div key={ripple.key} initial={{ opacity: 0.4 }} animate={{ opacity: 0.2 }} exit={{ opacity: 0 }}>
-              <RippleCircle key={"A" + ripple.key} initial={{ backgroundColor: "rgb(30, 30, 30)" }} animate={{ backgroundColor: "rgb(0,0,0)"}} exit={{ backgroundColor: "rgb(255, 255, 255)"}} style={ripple.styles} />
+            <motion.div key={ripple.key} transition={{duration: 2}} initial={{ opacity: 0.4 }} animate={{ opacity: ripple.vis ? 0.2 : 0 }}>
+              <RippleCircle key={`${ripple.key}a`} initial={{ backgroundColor: "rgb(30, 30, 30)" }} animate={{ backgroundColor: ripple.vis ? 'rgb(0,0,0)' : 'rgb(40, 40, 40)' }} style={ripple.styles} />
             </motion.div>
           ))}
-        </AnimatePresence>
       </WrapperInner>
     </Wrapper>
   );
--- src/Utils/UseCancelTimeouts.tsx
+++ src/Utils/UseCancelTimeouts.tsx
@@ -1,24 +1,25 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 
-type clearTimeout_t =  string | number | NodeJS.Timeout | undefined;
+type clearTimeout_t = string | number | NodeJS.Timeout | undefined;
 
 export const useCancellables = () => {
-    const [ids, setIds] = useState<clearTimeout_t[]>([])
+  const [ids, setIds] = useState<clearTimeout_t[]>([]);
 
-    React.useEffect(() => {
-        return () => {
-            for(let fn in ids) {
-                clearTimeout(fn);
-            }
-        };
-    }, []);
+  React.useEffect(
+    () => () => {
+      for (const id of ids) {
+        clearTimeout(id);
+      }
+    },
+    [],
+  );
 
-    const addId = (id: clearTimeout_t) => setIds([...ids, id]);
-    const addTimeout = (cb: Function, d: number) => addId(setTimeout(cb,d));
+  const addId = (id: clearTimeout_t) => setIds([...ids, id]);
+  const addTimeout = (cb: Function, d: number) => { addId(setTimeout(cb, d)); };
 
-    const handleStates = (states: [Function, number][]) => {
-        states.forEach(([cb,d]) => addTimeout(cb,d));
-    }
+  const handleStates = (states: [Function, number][]) => {
+    states.forEach(([cb, d]) => addTimeout(cb, d));
+  };
 
-    return handleStates;
+  return handleStates;
 };
